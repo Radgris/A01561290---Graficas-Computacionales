e3952eda52d464f05510b57a230e6f2705caf314
Radgris <golden_run@hotmail.com>
2020-07-15 17:17:08 -0600

    octahedron done
    tarea1

--- tarea 1/tarea1.js
+++ tarea 1/tarea1.js
@@ -356,7 +356,312 @@ function createPyramid(gl, scale, translation, rotationAxis) {
     return pyramid;
 };
 
-function createDodecahedron(gl, scale, translation, rotationAxis) {};
+function createDodecahedron(gl, scale, translation, rotationAxis) {
+    // Vertex Data
+let vertexBuffer;
+vertexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+let x = scale;
+console.log("dodecahedron scale is : " + x)
+
+phi = (1 + Math.sqrt(5)) / 2
+
+
+
+let aux_verts = [
+    //orange cube
+
+    -1, 1, 1,
+    1, 1, 1,
+    1, -1, 1,
+    -1, -1, 1,
+
+    -1, 1, -1,
+    1, 1, -1,
+    1, -1, -1,
+    -1, -1, -1,
+
+    //green rectangle
+
+    0, 1 / phi, phi,
+    0, -1 / phi, phi,
+    0, 1 / phi, -phi,
+    0, -1 / phi, -phi,
+
+    //blue rectangle
+
+    -1 / phi, phi, 0,
+    1 / phi, phi, 0,
+    -1 / phi, -phi, 0,
+    1 / phi, -phi, 0,
+
+    //pink rectangle
+
+    -phi, 0, 1 / phi,
+    phi, 0, 1 / phi,
+    -phi, 0, -1 / phi,
+    phi, 0, -1 / phi
+
+]
+
+
+let verts = [
+
+    //p1 g1-og1-og2-b1-b2
+
+    0, 1 / phi, phi,
+    -1, 1, 1,
+    1, 1, 1,
+    -1 / phi, phi, 0,
+    1 / phi, phi, 0,
+
+    //p2 g3-og5-og6-b1-b2
+    
+    0, 1 / phi, -phi,
+    -1, 1, -1,
+    1, 1, -1,    
+    -1 / phi, phi, 0,
+    1 / phi, phi, 0,
+
+    //p3 g4-og7-og8-b4-b3
+
+    0, -1 / phi, -phi,
+    1, -1, -1,
+    -1, -1, -1,
+    1 / phi, -phi, 0,
+    -1 / phi, -phi, 0,
+
+    //p4 g2-og3-og4-b4-b3
+
+    0, -1 / phi, phi,
+    1, -1, 1,
+    -1, -1, 1,
+    1 / phi, -phi, 0,
+    -1 / phi, -phi, 0,
+    
+    //p5 p1-og1-og4-g1-g2
+
+    -phi, 0, 1 / phi,
+    -1, 1, 1,
+    -1, -1, 1,
+    0, 1 / phi, phi,
+    0, -1 / phi, phi,
+
+    //p6 p2-og2-og3-g1-g2
+
+    phi, 0, 1 / phi,
+    1, 1, 1,
+    1, -1, 1,
+    0, 1 / phi, phi,
+    0, -1 / phi, phi,
+
+    //p7 p3-og5-og8-g3-g4
+
+    -phi, 0, -1 / phi,
+    -1, 1, -1,
+    -1, -1, -1,
+    0, 1 / phi, -phi,
+    0, -1 / phi, -phi,
+
+    //p8 p4-og6-og7-g3-g4
+
+    phi, 0, -1 / phi,
+    1, 1, -1,
+    1, -1, -1,
+    0, 1 / phi, -phi,
+    0, -1 / phi, -phi,
+
+    //p9 b1-og1-og5-p1-p3
+
+    -1 / phi, phi, 0,
+    -1, 1, 1,
+    -1, 1, -1,
+    -phi, 0, 1 / phi,
+    -phi, 0, -1 / phi,
+
+
+
+    //p10 b3-og4-og8-p1-p3
+
+    -1 / phi, -phi, 0,
+    -1, -1, 1,
+    -1, -1, -1,
+    -phi, 0, 1 / phi,
+    -phi, 0, -1 / phi,
+
+    //p11 b2-og2-og6-p2-p4
+
+    1 / phi, phi, 0,
+    1, 1, 1,
+    1, 1, -1,
+    phi, 0, 1 / phi,
+    phi, 0, -1 / phi,
+
+    //p12 b4-og3-og7-p2-p4
+
+    1 / phi, -phi, 0,
+    1, -1, 1,
+    1, -1, -1,
+    phi, 0, 1 / phi,
+    phi, 0, -1 / phi,
+
+    
+
+];
+
+
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+// Color data
+let colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+let faceColors = [
+
+    [0.0, 0.0, 0.0, 1.0], // p1 
+    [0.0, 0.0, 1.0, 1.0], // p2 
+    [0.0, 1.0, 0.0, 1.0], // p3
+    [0.0, 1.0, 1.0, 1.0], // p4
+    [1.0, 0.0, 0.0, 1.0], // p5
+    [1.0, 0.0, 1.0, 1.0], // p6
+    [1.0, 1.0, 0.0, 1.0], // p7
+    [1.0, 1.0, 1.0, 1.0], // p8
+    [0.0, 0.0, 0.0, 1.0], // p9
+    [0.0, 0.0, 1.0, 1.0], // p10
+    [0.0, 1.0, 0.0, 1.0], // p11
+    [0.0, 1.0, 1.0, 1.0], // p12
+   
+
+
+];
+
+// Each vertex must have the color information, that is why the same color is concatenated 4 times, one for each vertex of the cube's face.
+let vertexColors = [];
+
+faceColors.forEach(color => {
+    for (let j = 0; j < 5; j++)
+        vertexColors.push(...color);
+});
+
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+// Index data (defines the triangles to be drawn).
+let dodecahedronIndexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, dodecahedronIndexBuffer);
+
+let dodecahedronIndices = [
+    
+    //p1
+    0,1,2,
+    1,2,3,
+    2,3,4,
+
+    //p2
+
+    5,6,7,
+    6,7,8,
+    7,8,9,
+
+    //p3
+
+    10,11,12,
+    11,12,13,
+    12,13,14,
+
+    //p4
+
+    15,16,17,
+    16,17,18,
+    17,18,19,
+
+    //p5
+
+    20,21,22,
+    21,22,23,
+    22,23,24,
+
+    //p6
+
+    25,26,27,
+    26,27,28,
+    27,28,29,
+
+    //p7
+
+    30,31,32,
+    31,32,33,
+    32,33,34,
+    
+    //p8
+
+    35,36,37,
+    36,37,38,
+    37,38,39,
+
+    //p9
+    40,41,42,
+    41,42,43,
+    42,43,44,
+
+    //p10
+
+    45,46,47,
+    46,47,48,
+    47,48,49,
+
+    //p11
+
+    50,51,52,
+    51,52,53,
+    52,53,54,
+
+    //p12
+
+    55,56,57,
+    56,57,58,
+    57,58,59,
+  
+];
+
+
+// gl.ELEMENT_ARRAY_BUFFER: Buffer used for element indices.
+// Uint16Array: Array of 16-bit unsigned integers.
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(dodecahedronIndices), gl.STATIC_DRAW);
+
+let dodecahedron = {
+    buffer: vertexBuffer,
+    colorBuffer: colorBuffer,
+    indices: dodecahedronIndexBuffer,
+    vertSize: 3,
+    nVerts: verts.length / 3,
+    colorSize: 4,
+    nColors: vertexColors.length / 4,
+    nIndices: dodecahedronIndices.length,
+    primtype: gl.TRIANGLES,
+    modelViewMatrix: mat4.create(),
+    currentTime: Date.now()
+};
+
+mat4.translate(dodecahedron.modelViewMatrix, dodecahedron.modelViewMatrix, translation);
+
+dodecahedron.update = function () {
+    let now = Date.now();
+    let deltat = now - this.currentTime;
+    this.currentTime = now;
+    let fract = deltat / duration;
+    let angle = Math.PI * 2 * fract;
+
+    // Rotates a mat4 by the given angle
+    // mat4 out the receiving matrix
+    // mat4 a the matrix to rotate
+    // Number rad the angle to rotate the matrix by
+    // vec3 axis the axis to rotate around
+    mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, rotationAxis);
+    mat4.rotate(this.modelViewMatrix, this.modelViewMatrix, angle, [0.0, 1.0, 0,0]);
+};
+
+return dodecahedron;
+};
 
 function createOctahedron(gl, scale, translation, rotationAxis) {
 
